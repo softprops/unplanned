9160d96caf66edacc2d690c36a1b7e5e15baf642
softprops <d.tangren@gmail.com>
2011-03-11 21:42:47 -0500

    pubTo

--- project/build/project.scala
+++ project/build/project.scala
@@ -6,4 +6,6 @@ class Project(info: ProjectInfo) extends DefaultProject(info)
   override def assemblyJarName = "unplanned.jar"
   // for conscript
   val launchInterface = "org.scala-tools.sbt" % "launcher-interface" % "0.7.4" % "provided"
+  override def managedStyle = ManagedStyle.Maven
+  lazy val publishTo = Resolver.file("lessis repo", new java.io.File("/var/www/repo"))
 }
